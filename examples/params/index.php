528a157fb4ecdc8d69be00392374a3ab7eb270ee
Ilan JOURNO <ilan.journo@nolina.io>
2024-03-25 15:39:30 +0100

    :sparkles: Add params

---
+++ examples/params/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mini Twitter</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <aside>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Explorer</a></li>
+                <li><a href="#">Notifications</a></li>
+                <li><a href="#">Messages</a></li>
+                <li><a href="#">Profil</a></li>
+            </ul>
+        </aside>
+        <section class="feed">
+            <form id="tweetForm">
+                <textarea placeholder="Quoi de neuf ?"></textarea>
+                <button type="submit">Tweeter</button>
+            </form>
+            <div class="tweets">
+           
+            </div>
+        </section>
+    </div>
+</body>
+</html>
