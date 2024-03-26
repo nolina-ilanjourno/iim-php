bef72716eeec06f2bf91dd0e67452c37c3734831
Ilan JOURNO <ilan.journo@nolina.io>
2024-03-26 14:36:13 +0100

    :bug: Debug correction

--- examples/structuration_correction/index.php
+++ examples/structuration_correction/index.php
@@ -1,29 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Mini Twitter</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div class="container">
-        <?php require_once 'layouts/menu.html'; ?>
-        <section class="feed">
-            <form id="tweetForm">
-                <textarea placeholder="Quoi de neuf ?"></textarea>
-                <button type="submit">Tweeter</button>
-            </form>
-            <div class="tweets">
-                <!-- Les tweets seront ajoutés ici -->
-                <?php
-                echo "<h3>$nombreTweets tweet(s) de $user</h3>";
+<?php
 
-                for ($i = 0; $i < $nombreTweets; $i++) {
-                    echo "<div class='tweet'>Tweet " . ($i + 1) . " de $user</div>";
-                }
-                ?>
-            </div>
-        </section>
-    </div>
-</body>
-</html>
+// Récupération des paramètres GET 'user' et 'tweets'
+$user = isset($_GET['user']) ? $_GET['user'] : 'Utilisateur inconnu';
+$nombreTweets = isset($_GET['tweets']) ? (int)$_GET['tweets'] : 1;
+
+require_once 'index.html.php';
